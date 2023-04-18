0c87c5b20498403894c82053588bf59d20dd80ff
DaniellePashayan <87950814+DaniellePashayan@users.noreply.github.com>
2023-04-18 11:11:31 -0400

    updated batch script to be dynamic

--- batch_script.bat
+++ batch_script.bat
@@ -1,3 +1,10 @@
 @Echo Starting File Move
-python -u "C:\Users\dpashayan\Desktop\Automations\HomeCare_File_Move\main.py"
+
+setlocal
+set "FILE_PATH=%~dp0.."
+set "SCRIPT_PATH=%FILE_PATH%\main.py"
+python -u "%SCRIPT_PATH%"
+endlocal
+
+python -u "%SCRIPT_PATH%"
 @Echo Process Completed
