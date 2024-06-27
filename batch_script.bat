fdf0f91892bc5d73df07f32c04295fa5e015970a
Danielle Pashayan <daniellepashayan@gmail.com>
2024-06-27 10:15:52 -0400

    added error handling in .bat

--- batch_script.bat
+++ batch_script.bat
@@ -4,7 +4,15 @@ echo Starting Home Care File Move
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%main.py
+set VENV_PATH=%FILE_PATH%.venv
+
+call "%VENV_PATH%\Scripts\activate.bat"
 python -u "%SCRIPT_PATH%"
+
+IF ERRORLEVEL 1 (
+    echo Python script encountered an error. The error message is:
+    pause
+)
 ENDLOCAL
 
 Echo Process Completed
