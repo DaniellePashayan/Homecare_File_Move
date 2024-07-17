6afccced6ec19f78fab543b3cd10198d231fd48b
Danielle Pashayan <daniellepashayan@gmail.com>
2024-07-17 09:45:29 -0400

    converted to unc path for server

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
+from loguru import logger
 from functions import delete_empty_folders, move_files, correct_folder_names
 
 global src_path
 global dest_path
 
-dest_path = 'S:/NewRefCenter/ANewReferralPHI/NS/'
-src_path = f'{dest_path}/BOT/Medical Records'
+dest_path = '\\\\NASHCN01\\SHAREDATA\\NewRefCenter\\ANewReferralPHI\\NS\\'
+src_path = f'{dest_path}\\BOT\\Medical Records'
 
 
 def run():
     global src_path
     global dest_path
+    
+    logger.add(f'.\\logs\\log.log', rotation='1 week', retention='10 days', level='INFO')
     correct_folder_names(src_path)
     for folder in os.listdir(src_path):
         if (len(os.listdir(src_path))) > 0:
@@ -20,6 +23,7 @@ def run():
             move_files(path)
 
     delete_empty_folders(src_path)
+    logger.success('Process completed')
 
 if __name__ == '__main__':
     run()
