9a7609aefc8d780764e0550f7896f0e526339b48
Danielle Pashayan <daniellepashayan@gmail.com>
2025-09-23 10:15:38 -0400

    added more logging

--- src/folder.py
+++ src/folder.py
@@ -30,7 +30,7 @@ class Folder:
         self.get_dated_folder()
         return [file for file in os.listdir(self.dated_folder) if '.pdf' in file]
     
-    def convert_files_to_df(date:datetime.datetime, file_list:list):
+    def convert_files_to_df(self, date:datetime.datetime, file_list:list):
         dict_list = {date.strftime('%m_%d_%y'): file_list}
         
         df = [(key, value) for key, values in dict_list.items() for value in values]
@@ -40,33 +40,36 @@ class Folder:
         csv_path = f'./logs/trackers/{self.date.strftime("%Y")}/{self.date.strftime("%m %Y")}'
         os.makedirs(csv_path, exist_ok=True)
         df.to_csv(f'{csv_path}/{self.date.strftime("%m-%d-%y")}.csv', index=False)
-        return self.df
     
     def copy_files_to_destination(self):
         file_list = self.list_files()
+        self.convert_files_to_df(self.date, file_list)
         files_moved = 0
         
         for file in file_list:
-            source_file = os.path.join(dated_folder, file)
+            source_file = os.path.join(self.dated_folder, file)
             destination_file = os.path.join(self.destination_path, file)
             try:
-                # shutil.copy(source_file, destination_file)
+                shutil.copy(source_file, destination_file)
                 # check if file exists in destination
                 if os.path.exists(destination_file):
-                    file_df.loc[file_df['file'] == file, 'moved'] = True
-                files_moved += 1
-                logger.info(f'Copied {file} to {self.destination_path}')
+                    self.df.loc[self.df['file'] == file, 'moved'] = True
+                    files_moved += 1
+                    logger.debug(f'Copied {file} to {self.destination_path}')
             except Exception as e:
                 logger.error(f'Error copying {file}: {e}')
                 send_error_notification(f'HomeCare - Error copying {file}: {e}')
         return files_moved
     
     def archive_folder(self):
+        logger.info(f'Archiving folder {self.dated_folder}')
         archive_folder = os.path.join(self.source_path, 'ARCHIVE')
         # once the files are copied, zip the folder and move to archive
         if not os.path.exists(archive_folder):
             os.makedirs(archive_folder)
         shutil.make_archive(self.dated_folder, 'zip', self.dated_folder)
+        logger.info(f'Created zip file {self.dated_folder}.zip')
         shutil.move(f'{self.dated_folder}.zip', os.path.join(archive_folder, f'{os.path.basename(self.dated_folder)}.zip'))
         # remove the original folder and files
         shutil.rmtree(self.dated_folder)
+        logger.success(f'Archived and removed folder {self.dated_folder}')
