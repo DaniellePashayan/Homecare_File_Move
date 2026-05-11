d601ec36fee24e7fecc371a8618006973e9e00b7
Danielle Pashayan <daniellepashayan@gmail.com>
2026-05-11 13:42:11 -0400

    remoed pushbullet

--- main.py
+++ main.py
@@ -5,27 +5,14 @@ import os
 import shutil
 import re
 from loguru import logger
-from pushbullet import Pushbullet
-from orcca.status_handler import JSONStatus
 
 
 #setup logging
 logger.add(f'./logs/log.log', rotation='1 week', retention='10 days', level='INFO')
-status = JSONStatus(
-        master_file_path=r'\\NT2KWB972SRV03\SHAREDATA\CPP-Data\CBO Westbury Managers\LEADERSHIP\Bot Folder\Automated Scripts Status.json',
-        process_name='Homecare File Move'
-    )
 
-class Notifier:
-    def __init__(self, api_key):
-        self.pb = Pushbullet(api_key)
-    
-    def notify(self, title, message):
-        self.pb.push_note(title, message)
 
 class Folder:
     def __init__(self):
-        status.update_status('Running')
         self.destination_path = r'//NASHCN01/SHAREDATA/NewRefCenter/ANewReferralPHI/NS'
         self.source_path = r'//NASHCN01/SHAREDATA/NewRefCenter/ANewReferralPHI/NS/BOT/Medical Records'
         
@@ -55,11 +42,8 @@ class Folder:
         if len(os.listdir(os.path.join(self.source_path, folder_name))) == 0:
             logger.debug(f'Deleting empty folder {folder_name}')
             os.rmdir(os.path.join(self.source_path, folder_name))
-            status.update_status('Completed')
         else:
             logger.warning(f'Folder {folder_name} is not empty, skipping deletion')
-            notifier.notify('Homecare File Move', f'Folder {folder_name} is not empty, skipping deletion')
-            status.update_status('Error', f'Folder {folder_name} is not empty, skipping deletion')
 
 
     def move_files(self):
@@ -79,15 +63,12 @@ class Folder:
                             self.file_tracker.loc[self.file_tracker['file'] == file, 'moved'] = True
                         else:
                             logger.error(f'{file} from {folder_name} said it was moved to {self.destination_path}, but was not.')
-                            notifier.notify('Homecare File Move', f'{file} from {folder_name} said it was moved to {self.destination_path}, but was not.')
                     else:
                         logger.warning(f'File {file} already exists in {self.destination_path}')
 
                 self.delete_empty_folders(folder_name)
         except Exception as e:
             logger.error(f'Error moving {file}: {e}')
-            status.update_status('Error', e)
-            notifier.notify('Homecare File Move', f'Error moving {file}: {e}')
 
             
     def write_csv(self):
@@ -97,8 +78,6 @@ class Folder:
             logger.success('All files moved successfully')
         else:
             logger.error('Not all files were moved successfully')
-            status.update_status('Error', 'Not all files were moved successfully')
-            notifier.notify('Homecare File Move', 'Not all files were moved successfully')
         os.makedirs('./logs/trackers', exist_ok=True)
         self.file_tracker.to_csv(f'./logs/trackers/{today}.csv', index=False)
 
@@ -110,5 +89,4 @@ if __name__ == '__main__':
         else:
             folder.move_files()
             folder.write_csv()
-            status.update_status('Completed')
             break
